1ec33e7cdd0205565d4b8e065124af88d0281232
Dominik Wiśniewski <wisniewski.dominik@gmail.com>
2026-04-09 10:55:26 +0200

    Mvn verify stage

---
+++ src/main/java/com/example/demo/TestRestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
