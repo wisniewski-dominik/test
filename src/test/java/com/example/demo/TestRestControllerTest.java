81a2f4c40c98c1f38cbec02433e431798f3fa57b
Dominik Wiśniewski <wisniewski.dominik@gmail.com>
2026-04-09 12:45:07 +0200

    More tests to allow JaCoCo rules to pass

--- src/test/java/com/example/demo/TestRestControllerTest.java
+++ src/test/java/com/example/demo/TestRestControllerTest.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class TestRestControllerTest {
     @Test
     void get() {
